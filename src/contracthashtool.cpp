e5761d5b2eab87cfdf6450073fd8cd60bea53a36
Gregory Sanders <gsanders87@gmail.com>
2016-02-05 15:38:31 -0500

    Only create context once

--- src/contracthashtool.cpp
+++ src/contracthashtool.cpp
@@ -16,6 +16,9 @@
 #include <secp256k1_ecdh.h>
 #include <secp256k1_schnorr.h>
 
+secp256k1_context_t *secp256k1_ctx_cht = NULL;
+
+
 int get_pubkeys_from_redeemscript(unsigned char *redeem_script, unsigned int redeem_script_len, unsigned char* pubkeys[]) {
 	unsigned char *readpos = redeem_script, * const endpos = redeem_script + redeem_script_len;
 	unsigned char *maybe_keys[redeem_script_len/33];
@@ -132,16 +135,16 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
         return json_spirit::Value::null;
     }
 
-    //Need to instantiate elsewhere, or use pubkey.cpp's instance. 
-	secp256k1_context_t *secp256k1_ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+    if (secp256k1_ctx_cht == NULL) {
+        secp256k1_ctx_cht = secp256k1_context_create(SECP256K1_CONTEXT_SIGN|SECP256K1_CONTEXT_VERIFY);
+    }
 
-	// GLOBALCONV
+    // GLOBALCONV
 	unsigned char p2sh_bytes[20];
 	const char* address_type = "TEXT";
 	if (p2sh_address) {
         CBitcoinAddress addr(p2sh_address);
         if (!addr.IsValid()) {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
         CTxDestination dest = addr.Get();
@@ -153,7 +156,6 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
             memcpy(p2sh_bytes, boost::get<CScriptID>(dest).begin(), 20);
         }
         else {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
         address_type = (addr.IsScript() ? "P2SH" : "P2PH");
@@ -166,7 +168,6 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
 	if (fullcontract_hex) {
 		unsigned char fullcontract[40];
 		if (!hex2bytes(fullcontract_hex, fullcontract, 40)) {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
 		if (memcmp(fullcontract, "P2SH", 4) == 0)
@@ -174,7 +175,6 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
 		else if (memcmp(fullcontract, "P2PH", 4) == 0)
 			address_type = "P2SH";
 		else {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
 
@@ -192,13 +192,11 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
 		unsigned int redeem_script_len = strlen(redeem_script_hex)/2;
 		unsigned char redeem_script[redeem_script_len];
 		if (!hex2bytes(redeem_script_hex, redeem_script, redeem_script_len)) {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
 		unsigned char* keys[redeem_script_len / 33];
 		int key_count = get_pubkeys_from_redeemscript(redeem_script, redeem_script_len, keys);
 		if (key_count < 1) {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
 	
@@ -224,20 +222,18 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
 				unsigned char res[32];
                 CHMAC_SHA256(keys_work[i], 33).Write(data, 4 + 16 + targetLen).Finalize(res);
 				secp256k1_pubkey_t pubkey;
-				if (!secp256k1_ec_pubkey_parse(secp256k1_ctx, &pubkey, keys_work[i], 33)) {
-	                secp256k1_context_destroy(secp256k1_ctx);
+				if (!secp256k1_ec_pubkey_parse(secp256k1_ctx_cht, &pubkey, keys_work[i], 33)) {
                     return json_spirit::Value::null;
                 }
                 // If tweak puts key over order, break or exit depending on fixed nonce
-				if (secp256k1_ec_pubkey_tweak_add(secp256k1_ctx, &pubkey, res) == 0) {
+				if (secp256k1_ec_pubkey_tweak_add(secp256k1_ctx_cht, &pubkey, res) == 0) {
 					if (nonce_hex) {
-	                    secp256k1_context_destroy(secp256k1_ctx);
                         return json_spirit::Value::null;
                     }
 					break;
 				}
 				int len = 33;
-				secp256k1_ec_pubkey_serialize(secp256k1_ctx, keys_work[i], &len, &pubkey, 1);
+				secp256k1_ec_pubkey_serialize(secp256k1_ctx_cht, keys_work[i], &len, &pubkey, 1);
 				assert(len == 33);
 			}
             // Break if all keys have been successfully tweaked
@@ -270,7 +266,6 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
         CBitcoinSecret secret;
         secret.SetString(priv_key_str);
         if (!secret.IsValid()) {
-	        secp256k1_context_destroy(secp256k1_ctx);
             return json_spirit::Value::null;
         }
         std::vector<unsigned char> vPriv;
@@ -289,19 +284,17 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
 
         // Gen public key from private
 		int len = 0;
-		if (secp256k1_ec_pubkey_create(secp256k1_ctx, &pubkey, priv) != 1) {
-	        secp256k1_context_destroy(secp256k1_ctx);
+		if (secp256k1_ec_pubkey_create(secp256k1_ctx_cht, &pubkey, priv) != 1) {
             return json_spirit::Value::null;
         }
-		secp256k1_ec_pubkey_serialize(secp256k1_ctx, pub, &len, &pubkey, 1);
+		secp256k1_ec_pubkey_serialize(secp256k1_ctx_cht, pub, &len, &pubkey, 1);
 		assert(len == 33);
 
         // Gen new private key by tweaking based on commitment to pubkey and data
 		unsigned char tweak[32];
         CHMAC_SHA256(pub, 33).Write(data, 4 + 16 + targetLen).Finalize(tweak);
 
-		if (secp256k1_ec_privkey_tweak_add(secp256k1_ctx, priv, tweak) != 1) {
-	        secp256k1_context_destroy(secp256k1_ctx);
+		if (secp256k1_ec_privkey_tweak_add(secp256k1_ctx_cht, priv, tweak) != 1) {
             return json_spirit::Value::null;
         }
 
@@ -310,11 +303,9 @@ json_spirit::Value contract_hashtool(const char mode, const char* redeem_script_
         privKey.Set(priv, priv+33, true);
         ret.push_back(Pair("priv", CBitcoinSecret(privKey).ToString()));
 	} else {
-	    secp256k1_context_destroy(secp256k1_ctx);
         return json_spirit::Value::null;
     }
-	secp256k1_context_destroy(secp256k1_ctx);
-
-	return ret;
+	
+    return ret;
 }
 
